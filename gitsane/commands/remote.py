dd4ff5634da6c00d0833fd1ffb15a1c1588a6605
Jesper Wendel Devantier <j.devantier@samsung.com>
2021-02-03 19:07:00 +0100

    document and show help for remote cmds

--- gitsane/commands/remote.py
+++ gitsane/commands/remote.py
@@ -6,25 +6,29 @@ app = typer.Typer(no_args_is_help=True)
 
 @app.command(name="list")
 def remote_list(verbose: bool = typer.Option(True, help="show fetch & push urls")):
+    """display a list of configured remotes"""
     if verbose:
         run(["git", "remote", "--verbose"], dry_run=False)
     else:
         run(["git", "remote"], dry_run=False)
 
 
-@app.command(name="add")
+@app.command(name="add", no_args_is_help=True)
 def remote_add(name: str = typer.Argument(..., help="name of remote"),
                url: str = typer.Argument(..., help="URL of remote")):
+    """add remote to repository"""
     run(["git", "remote", "add", name, url])
 
 
-@app.command(name="remove")
+@app.command(name="remove", no_args_is_help=True)
 def remote_remove(remote: str = typer.Argument(..., help="name of remote")):
+    """remove remote from repository"""
     run(["git", "remote", "remove", remote])
 
 
-@app.command(name="rename")
+@app.command(name="rename", no_args_is_help=True)
 def remote_rename(remote: str = typer.Argument(..., help="name of existing remote"),
                   new: str = typer.Argument(..., help="new name of remote")):
+    """change the name used to refer to remote"""
     """rename remote (remote rename <remote> <new-name>)"""
     run(["git", "remote", "rename", remote, new])
